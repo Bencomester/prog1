e2b8ccb3c6e6c1c31cafc2db74558083c2064dd7
Bencomester <141187261+Bencomester@users.noreply.github.com>
2025-02-24 15:37:27 +0100

    Add files via upload

---
+++ max_ram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    while (true) {
+        try {
+            char* p = new char[1024 * 1024];
+        }
+        catch (bad_alloc&) {
+            cerr << "Failed";
+        }
+    }
+}
+
