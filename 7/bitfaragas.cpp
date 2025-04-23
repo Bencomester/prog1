6ea17fa3c0b0c2267f24b7bfaf7684989b0e679f
Bencomester <bencomester@gmail.com>
2025-04-23 17:44:30 +0200

    Reduced size from 768B to 676B

--- 7/bitfaragas.cpp
+++ 7/bitfaragas.cpp
@@ -4,25 +4,24 @@
 #include <cctype>
 #include <string>
 using namespace std;
-bool p(string t) {
+bool p(string t){
 	string s;
     for(char c:t)if(isalpha(c))s+=tolower(c);
     for(int l=0,r=s.size()-1;l<r;l++,r--)if(s[l]!=s[r])return 0;
     return 1;
 }
-int main() {
+int main(){
     string s;
-    cout<<"Szót a palindróm ellenőrzéshez: ";
+    cout<<"Szöveg: ";
     getline(cin,s);
-    cout<<"A megadott szöveg "<<(p(s)?"palindróm.\n":"nem palindróm.\n");
+    cout<<"A szöveg "<<(p(s)?"palindróm.\n":"nem palindróm.\n");
     int n;
-    cout<<"\nHány számot szeretnél rendezni? ";
+    cout<<"\nSzámok száma: ";
     cin>>n;
-    cout<<"Add meg a számokat:\n";
+    cout<<"Számok:\n";
     vector<int> v(n);
     for(int i=0;i<n;i++){cout<<i+1<<". ";cin>>v[i];}
     sort(v.begin(),v.end());
-    cout<<"Rendezett tömb: ";
+    cout<<"Rendezett: ";
     for(int i:v)cout<<i<<" ";
-    cout<<"\n";
 }
