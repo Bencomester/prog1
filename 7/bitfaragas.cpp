97c3c3c103ea1152d7fba75899a0ef5fe23062d0
Bencomester <bencomester@gmail.com>
2025-04-24 22:06:52 +0200

    Reduced size from 346B to 310B

--- 7/bitfaragas.cpp
+++ 7/bitfaragas.cpp
@@ -1,2 +1,2 @@
 #include <bits/stdc++.h>
-using namespace std;int main(){ostream&o=cout;string t,s;o<<"Szöveg: ";getline(cin,t);for(char c:t)if(isalpha(c))s+=tolower(c);o<<(s==string(s.rbegin(),s.rend())?"P":"Nem p")<<"alindróm\n";int n,j;o<<"\nHány szám? ";cin>>n;multiset<int>v;for(int i=1;i<=n;i++){o<<i<<". ";cin>>j;v.insert(j);}for(int i:v)o<<i<<" ";}
+using namespace std;int main(){string t,s;cout<<"text: ";getline(cin,t);for(char c:t)if(isalpha(c))s+=tolower(c);cout<<(s==string(s.rbegin(),s.rend())?"":"nem ")<<"palindróm\n";int n,j;cout<<"\nhány? ";multiset<int>v;for(cin>>n;n--;){cin>>j;v.insert(j);}for(int i:v)cout<<i<<" ";}
